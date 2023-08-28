28e50bd64e29f5b4f9c3fe1eb453ef6d2b389c61
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-08-28 17:18:30 -0300

    att

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,5 +1,8 @@
 import Image from "next/image"
 import logo from "../../../public/logo.png"
+import visa from "../../../public/visa.png"
+import master from "../../../public/master.png"
+import american from "../../../public/american.png"
 import rixxer from "../../../public/rixxer.png"
 import Link from "next/link"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -12,60 +15,67 @@ export default function Footer() {
 
     return (
         <div className="flex flex-col justify-center items-center w-screen">
-            <div className="bg-primary p-16 md:p-2 md:py-5 flex md:flex-col justify-around w-screen text-light md:space-y-5">
-                <div className="flex flex-col justify-center items-center mb-10 md:mb-0">
-                    <Image src={logo} className="w-48" />
+            <div className="bg-primary p-16 md:p-2 md:py-5 flex md:flex-col justify-center gap-16 w-screen text-light md:space-y-5">
+                <div className="flex flex-col  mb-10 md:mb-0 ">
+                    <Image src={logo} className="w-48"/>
                     <p className="w-64 md:w-auto text-justify text-sm mt-5 ">Descubra o incrível universo lúdico da Marcio Artezanatos! Loja especializada em produtos sustentáveis e produzidos por empresas brasileiras, resgatamos memórias e histórias, proporcionando aprendizagens significativas para todas as idades. Com jogos, brinquedos em madeira, livros e quebra-cabeças, estimulamos a imaginação, criatividade e o desenvolvimento integral das crianças. Venha explorar nosso mundo encantado e transforme o aprendizado em uma experiência mágica.</p>
-                </div>
-                <div className="flex flex-col mt-0 space-y-5 md:text-center">
-                    <h2 className="uppercase font-bold">Institucional</h2>
-                    <Link href="fale-conosco"
-                        className="hover:text-light transition-all duration-300">
-                        Fale Conosco
-                    </Link>
-                    <Link href="/politica-de-privacidade" className="hover:text-light transition-all duration-300">
-                        Política de Privaciade
-                    </Link>
-                    <Link href="politica-de-troca-e-devolucao" className="hover:text-light transition-all duration-300">
-                        Nossas Políticas de troca e devolução
-                    </Link>
-                    <Link href="quem-somos"
-                        className="hover:text-light transition-all duration-300">
-                        Quem Somos?
-                    </Link>
-                </div>
-
-                <div className="flex flex-col justify-start md:items-center text-center">
-                    <Link href="https://wa.me/5535998647172" target="_blank" className="flex hover:text-light transition-all duration-300">
-
-                        <FontAwesomeIcon icon={faWhatsapp} className="text-2xl h-6 mr-2" />
-                        +55 35 9864-7172
-                    </Link>
-                </div>
-
-                <div className="flex flex-col justify-center items-center gap-5">
-                </div>
-
-                <div className="flex flex-col md:justify-center md:items-center  space-y-5">
-                    <h2 className="uppercase font-bold">Redes Sociais</h2>
-                    <div className="flex gap-2">
-                        <Link href="https://www.instagram.com/queijofazendasantoantonio/" target="_blank" className="text-light text-2xl hover:text-light transition-all duration-300 w-5"><FontAwesomeIcon icon={faInstagram} />
+                    <div className="flex gap-5 mt-5">
+                        <Link href="https://www.instagram.com/marcio.artesanatos.sl/" target="_blank" className="text-light text-2xl hover:text-primaryDark transition-all duration-300 w-5"><FontAwesomeIcon icon={faInstagram} />
                         </Link>
-                        <Link href="https://www.facebook.com/queijofazendasantoantonio/" target="_blank" className="text-light text-2xl hover:text-light transition-all duration-300 w-6"><FontAwesomeIcon icon={faFacebook} />
+                        <Link href="https://www.facebook.com/profile.php?id=100069674319618" target="_blank" className="text-light text-2xl hover:text-primaryDark transition-all duration-300 w-6"><FontAwesomeIcon icon={faFacebook} />
                         </Link>
 
                     </div>
                 </div>
+                <div className="flex flex-col space-y-5 md:text-center">
+                    <h2 className="uppercase font-bold">Institucional</h2>
+                    <Link href="fale-conosco"
+                        className="hover:text-primaryDark transition-all duration-300 pt-8">
+                        Fale Conosco
+                    </Link>
+                    <Link href="/politica-de-privacidade" className="hover:text-primaryDark transition-all duration-300">
+                        Política de Privaciade
+                    </Link>
+                    <Link href="politica-de-troca-e-devolucao" className="hover:text-primaryDark transition-all duration-300">
+                        Nossas Políticas de troca e devolução
+                    </Link>
+                    <Link href="quem-somos"
+                        className="hover:text-primaryDark transition-all duration-300">
+                        Quem Somos?
+                    </Link>
+                </div>
 
+                <div className="flex flex-col justify-start md:items-center">
+                    <h2 className="w-72">
+                        Endereço Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam deserunt ab.
+                    </h2>
+                    <Link href="https://wa.me/5535998647172" target="_blank" className="flex  items-center hover:text-primaryDark transition-all duration-300 mt-5">
+
+                        <FontAwesomeIcon icon={faWhatsapp} className="text-2xl h-4 mr-2 " />
+                        +55 35 9864-7172
+                    </Link>
+                    <div className="flex">
+                        <Image src={visa} alt="imagem da bandeira visa"
+                            className="h-16 w-16" />
+                        <Image src={master} alt="imagem da bandeira visa"
+                            className="h-14 w-16" />
+                        <Image src={american} alt="imagem da bandeira visa"
+                            className="h-16 w-16" />
+                        
+                    </div>
+                </div>
+
+                <div className="flex flex-col justify-center items-center gap-5">
+                </div>
             </div>
             <div className="pt-5 text-light flex flex-col items-center justify-center text-center bg-primaryDark w-screen">
                 <p className="text-xs ">
-                    Fazenda-CNPJ: 0000000000000000000000000 Todos os Direitos Reservados, {ano}
+                    Marcio Artezanatos - CNPJ: 000000000000 Todos os Direitos Reservados, {ano}
                 </p>
                 <div className="flex">
                     <p className="text-sm">Developed by&nbsp; </p>{' '}
                     <a href="" className="font-bold text-light hover:text-secondary transition ease-in-out duration-300 flex justify-center items-center">
-                        <Image src={rixxer} alt="logo da emrpesa rixxer desenolvedora do site" className="w-14 mx-2"/>
+                        <Image src={rixxer} alt="logo da emrpesa rixxer desenolvedora do site" className="w-14 mx-2" />
                     </a>
                 </div>
             </div>
--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -31,17 +31,25 @@ export default function NavBar() {
   return (
     <div className="md:px-0 bg">
       <NavBarMobile />
-      <div className=" md:hidden flex justify-between items-center px-5 bg-zinc-100 relative">
+      <div className="fixed md:hidden  flex justify-between items-center px-5 bg-zinc-100 w-screen z-10 backdrop-blur-md  ">
         <div className="space-x-5 p-2 divide-primary/75 divide-x flex">
-          <Link href="#" className="text-black/75 hover:text-blue-500 transition-all duration-300 flex justify-center items-center"><FontAwesomeIcon icon={faInstagram} className="mr-2" />Instagram</Link>
-          <Link href="#" className="flex justify-center items-center text-black/75 hover:text-blue-500 transition-all duration-300 pl-5"><FontAwesomeIcon icon={faFacebook} className="mr-2" />Facebook</Link>
+          <Link href="https://www.instagram.com/marcio.artesanatos.sl/"
+          target="_blank" className="text-black/75 hover:text-blue-500 transition-all duration-300 flex justify-center items-center">
+            <FontAwesomeIcon icon={faInstagram} className="mr-2" />
+            Instagram
+          </Link>
+          <Link href="https://www.facebook.com/profile.php?id=100069674319618" 
+          target="_blank" className="flex justify-center items-center text-black/75 hover:text-blue-500 transition-all duration-300 pl-5">
+            <FontAwesomeIcon icon={faFacebook} className="mr-2" />
+            Facebook
+          </Link>
         </div>
 
         <div>
           <Link href="#" className="flex justify-center items-center hover:text-blue-500 transition-all duration-300 text-black/75"><FontAwesomeIcon icon={faWhatsapp} className="text-2xl mr-2" />+55 35 9121-6423</Link>
         </div>
       </div>
-      <div className="w-screen flex justify-center items-center">
+      <div className="w-screen flex justify-center items-center pt-10">
         <div className=" w-[1000px] flex justify-between items-center p-5 pb-5 md:hidden space-x-10">
           <Link href="/">
             <Image src={logo} alt="logo da empresa" className=" w-48" />
@@ -53,9 +61,14 @@ export default function NavBar() {
             </button>
           </div>
           <div className=" cursor-pointer">
-            <FontAwesomeIcon icon={faShoppingCart} className="text-primary h-5 p-4 hover:text-primaryDark transition-all duration-300" />
-            <FontAwesomeIcon icon={faUserCircle} className="text-primary h-5 p-4 hover:text-primaryDark transition-all duration-300" />
+            <Link href="/carrinho">
+              <FontAwesomeIcon icon={faShoppingCart} className="text-primary h-5 p-4 hover:text-primaryDark transition-all duration-300" />
+            </Link>
+            <Link href="/conta">
+              <FontAwesomeIcon icon={faUserCircle} className="text-primary h-5 p-4 hover:text-primaryDark transition-all duration-300" />
+            </Link>
           </div>
+
         </div>
       </div>
       <nav className="w-screen md:hidden flex justify-center gap-10 items-center pb-5 bg-light">
