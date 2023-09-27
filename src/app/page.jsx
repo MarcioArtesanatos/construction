37828897363adc0245f2a3ea709b3e1383e688dc
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-09-27 14:45:55 -0300

    pagination of prodructs created

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+import React, { useEffect, useState } from "react";
 import Card from './components/Card';
 import bgCrianca from "../../public/bgCrianca.jpg"
 import Link from 'next/link';
@@ -8,13 +9,13 @@ import Blog from './components/Blog';
 import Despoimentos from './components/Depoimentos';
 import Image from 'next/image';
 
-import React, { useEffect, useState } from "react"
 import { db } from "../../config/firebase"
 import { collection, getDocs } from 'firebase/firestore';
 
-// func do firebase
-function DatabaseRead() {
+function DatabaseRead({ currentPage, itemsPerPage }) {
   const [produto, setProduto] = useState([]);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
 
   useEffect(() => {
     async function fetchData() {
@@ -23,38 +24,34 @@ function DatabaseRead() {
           const dataCollection = collection(db, "produtos");
           const dataSnapshot = await getDocs(dataCollection);
           const dataList = dataSnapshot.docs.map((doc) => doc.data());
-          setProduto(dataList);
+          setProduto(dataList.slice(startIndex, endIndex)); // Filtra os itens da página atual
         }
         getProduto();
       } catch (error) {
         console.error("Erro:", error);
       }
     }
-    
+
     fetchData();
-  }, []);
-  
+  }, [currentPage]);
+
   return (
     <div className='flex flex-wrap'>
-
       {produto.map((item) => {
-         // Verifica se a propriedade "imagens" existe e é uma matriz não vazia
-      if (Array.isArray(item.imagens) && item.imagens.length > 0) {
-        // Pega o primeiro link (índice 0) da matriz de links
-        const primeiroLink = item.imagens[0];
-        console.log(item.id);
-        return (
-          <Card
-          title={item.produto}
-          preco={item.valor}
-          link={primeiroLink}
-          href="/compra"
-          />
-          
-        );
-        
- } })}
-     
+        if (Array.isArray(item.imagens) && item.imagens.length > 0) {
+          const primeiroLink = item.imagens[0];
+          console.log(item.id);
+          return (
+            <Card
+              key={item.id}
+              title={item.produto}
+              preco={item.valor}
+              link={primeiroLink}
+              href="/compra"
+            />
+          );
+        }
+      })}
     </div>
   );
 }
@@ -68,25 +65,56 @@ const CardIdade = ({ idade, diaMes, link }) => {
   )
 }
 
+const Pagination = ({ currentPage, totalPages, setCurrentPage }) => {
+  const pages = [...Array(totalPages).keys()].map((page) => page + 1);
+
+  return (
+    <div className="pagination space-x-5 ">
+      {pages.map((page) => (
+       <button
+       key={page}
+       onClick={() => setCurrentPage(page)}
+       className={`px-4 py-2 rounded-full mr-2 
+         ${currentPage === page ? 'bg-blue-500 text-white' : 'bg-white text-blue-500 hover:bg-blue-200'}
+       `}
+     >
+       {page}
+     </button>
+      ))}
+    </div>
+  );
+};
 
 export default function Home() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 1;
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    async function fetchTotalItems() {
+      try {
+        const dataCollection = collection(db, "produtos");
+        const dataSnapshot = await getDocs(dataCollection);
+        const totalItems = dataSnapshot.docs.length;
+        const calculatedTotalPages = Math.ceil(totalItems / itemsPerPage);
+        setTotalPages(calculatedTotalPages);
+      } catch (error) {
+        console.error("Erro ao obter total de itens:", error);
+      }
+    }
+
+    fetchTotalItems();
+  }, []);
+
   return (
     <div className="relative">
-      <div className='w-screen '>
-        {/* <Image src={bg} alt='nuven de fundo do site' className="absolute -top-11 -z-10 md:h-[300px] h-[1200px]"
-        />*/}
-
-      </div>
-
-      {/* <div className='hidden md:block w-screen h-[760px] -mt-[800px]'>
-        <CarrosselMobile />
-      </div>*/}
-      <div className='bg-black'>
-        {/*<Carrossel />*/}
+      <div className='w-screen'>
         <Image src={bgCrianca} alt='desenho de crianças brincando com brinquedos de madeira' className='opacity-70' />
       </div>
+
       <main className='bg-light w-full pt-10'>
         <div className='flex md:flex-col justify-center items-center gap-5 '>
+          {/* ... */}
 
           <div className=' mb-16 flex justify-center items-center border border-solid rounded-lg border-primaryDark px-10 py-5'>
             <FontAwesomeIcon icon={faTruckFast} className='h-10 text-blue-500 mr-2' />
@@ -125,15 +153,18 @@ export default function Home() {
               <CardIdade idade="9 - 12" diaMes="Anos" link="/por-idade" />
             </div>
 
-
-            {/* pagina de produtos */}
             <div className='bg-gradient-to-r w-screen mt-10'>
               <h2 className='uppercase mt-24 text-black/75 font-bold pb-10 text-4xl '>
                 Produtos
               </h2>
               <div className='py-5 flex flex-wrap justify-center'>
-                <DatabaseRead/>
+                <DatabaseRead currentPage={currentPage} itemsPerPage={itemsPerPage} />
               </div>
+              <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                setCurrentPage={setCurrentPage}
+              />
             </div>
             <h2 className='uppercase mt-24 text-black/75 font-bold py-10 text-4xl '>
               Mais comprados
@@ -144,24 +175,22 @@ export default function Home() {
           </div>
         </div>
       </main>
+
       <div className='flex flex-col w-screen justify-center items-center p-16 bg-primaryDark'>
         <h2 className='text-4xl text-white font-bold pb-10'>Depoimentos</h2>
         <div className='flex gap-5 flex-wrap justify-center'>
           <Despoimentos
             text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dolore quasi voluptas?"
-
             name="José lorem Y"
             cidade="São Lourenço"
           />
           <Despoimentos
             text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dolore quasi voluptas?"
-
             name="José lorem Y"
             cidade="São Lourenço"
           />
           <Despoimentos
             text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dolore quasi voluptas?"
-
             name="José lorem Y"
             cidade="São Lourenço"
           />
